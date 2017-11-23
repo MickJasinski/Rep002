67aabeafa750f74ec53ad0bb0f2bca5c99cab8c2
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-11-23 12:53:31 +0000

    Add training variables

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,11 @@ console.log('Yo, ' + name);
 /* var name = prompt('Tell me your name.');
 alert('Hi, ' + name);
 console.log('Hi, ' + name); */
+
+
+var a = 2,
+    h = 3,
+    triangleArea = a * h / 2;
+
+var display = alert(triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
