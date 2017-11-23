6dc09ea781d000ad2fddf42c8853285ca18f863d
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-11-23 10:33:58 +0000

    Add comments in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // scripts.js
 
+
+// Komentarz liniowy
+
+/* Komentarz
+blokowy */
+
 var name = prompt('Whutz ur name bro?');
 alert('Yo, ' + name);
 console.log('Yo, ' + name);
+
+/* var name = prompt('Tell me your name.');
+alert('Hi, ' + name);
+console.log('Hi, ' + name); */
