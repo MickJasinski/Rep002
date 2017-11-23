4b22fe9a45068669fe6cc06af7ca5e32be9ed6f8
gaap <baq@lost-sounds.com>
2017-11-23 18:06:53 +0000

    Change 'alert' to 'prompt'

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,5 @@ var a = 2,
     h = 3,
     triangleArea = a * h / 2;
 
-var display = alert(triangleArea);
+var a = prompt(triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
