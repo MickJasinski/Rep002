9d0bd64dd9ad96925d24012a3ef8a0c405590880
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-11-25 12:45:59 +0000

    Update with task 8.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
 // scripts.js
 
 
+var a = prompt('Value "A"'),
+    b = prompt('Value "B"'),
+    result = a * a - 2 * a * b + b * b;
+alert('The result is:' + result);
+console.log('The result is:' + result);
+
+if (result > 0) {
+	console.log('The result is positive');
+} else if (result < 0) {
+	console.log('The result is negative');
+} else {
+	console.log('The result is "zero"');
+}
+
+
+
 // Komentarz liniowy
 
 /* Komentarz
 blokowy */
 
-var name = prompt('Whutz ur name bro?');
-alert('Yo, ' + name);
-console.log('Yo, ' + name);
+// var name = prompt('Whutz ur name bro?');
+// alert('Yo, ' + name);
+// console.log('Yo, ' + name);
 
 /* var name = prompt('Tell me your name.');
 alert('Hi, ' + name);
 console.log('Hi, ' + name); */
 
 
-var a = prompt("Type the triangle's side length" + name),
-    h = prompt("Type the triangle's side height" + name),
-    triangleArea = a * h / 2;
-alert('Triangle area is:' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+// var a = prompt("Type the triangle's side length" + name),
+//     h = prompt("Type the triangle's side height" + name),
+//     triangleArea = a * h / 2;
+// alert('Triangle area is:' + triangleArea);
+// console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
